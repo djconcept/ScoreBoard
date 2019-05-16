1a6f5f6b011bed30498f53de6299633613927991
djconcept <dju@djconcept.be>
2019-05-16 15:09:44 +0200

    Add files via upload

--- KeyboardScoreBoard.ino
+++ KeyboardScoreBoard.ino
@@ -2,6 +2,31 @@
 
 #define NBR_TCH 11
 int myPins[NBR_TCH] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+
+#define B_RED_Bottom      0
+#define B_WHITE_Bottom    1
+#define B_BLACK           2
+#define B_BLUE            3
+#define B_RED_Left        4
+#define B_GREEN_Left      5
+#define B_GREEN_Right     6
+#define B_RED_Right       7
+#define B_YELLOW_Left     8
+#define B_WHITE_Top       9
+#define B_YELLOW_Right    10
+
+#define K_NUMPAD_1        225
+#define K_NUMPAD_2        226
+#define K_NUMPAD_3        227
+#define K_NUMPAD_4        228
+#define K_NUMPAD_5        229
+#define K_NUMPAD_6        230
+#define K_NUMPAD_7        231
+#define K_NUMPAD_8        232
+#define K_NUMPAD_9        233
+#define K_NUMPAD_0        234
+
+  
 int buttonState[NBR_TCH];
 int previousButtonState[NBR_TCH];
 
@@ -22,7 +47,7 @@ void loop() {
   {
     buttonState[i] = digitalRead(myPins[i]);
 
-    if (i == 9){
+    if (i == B_WHITE_Top){
       if (buttonState[i] == LOW){
         Keyboard.press(KEY_LEFT_CTRL);     
       }
@@ -35,51 +60,48 @@ void loop() {
     if ((buttonState[i] != previousButtonState[i]) && (buttonState[i] == LOW))
     {
       switch (i) {
-
-      
-        case 0:
-        Keyboard.write(234); //NUMPAD_0
+        case B_RED_Bottom:
+        Keyboard.write(K_NUMPAD_0);
         break;
       
-        case 1:
-        Keyboard.write(232); //NUMPAD_8
+        case B_WHITE_Bottom:
+        Keyboard.write(K_NUMPAD_2);
         break;
   
-        case 2:
-        Keyboard.press(KEY_LEFT_CTRL);   
-        Keyboard.write(232); //NUMPAD_8
+        case B_BLACK:
+        Keyboard.write(K_NUMPAD_5);   
         break;
   
-        case 3:
-        Keyboard.write(229); //NUMPAD_5
+        case B_BLUE:
+        Keyboard.write(K_NUMPAD_8);   
         break;
         
-        case 4:
-        Keyboard.write(228); //NUMPAD_4
+        case B_RED_Left:
+        Keyboard.write(K_NUMPAD_1);   
         break;
         
-        case 5:
-        Keyboard.write(231); //NUMPAD_7
+        case B_GREEN_Left:
+        Keyboard.write(K_NUMPAD_4);
         break;
         
-        case 6:
-        Keyboard.write(233); //NUMPAD_9
+        case B_GREEN_Right:
+        Keyboard.write(K_NUMPAD_6);
         break;
         
-        case 7:
-        Keyboard.write(230); //NUMPAD_6
+        case B_RED_Right:
+        Keyboard.write(K_NUMPAD_3);
         break;
 
-        case 8:
-        Keyboard.write(225); //NUMPAD_1
+        case B_YELLOW_Left:
+        Keyboard.write(K_NUMPAD_7);
         break;
         
-        case 9:
-        Keyboard.write(226); //NUMPAD_2
+        case B_WHITE_Top:
+        
         break;
 
-        case 10:
-        Keyboard.write(227); //NUMPAD_3
+        case B_YELLOW_Right:
+        Keyboard.write(K_NUMPAD_9);
         break;
       }
     }
